e248ac3bf6db7f8f0ad60cc913776aab666d1784
Pritam Kundu <75939390+warmachine028@users.noreply.github.com>
2024-09-16 03:43:05 +0530

    Update take-snapshot.mjs
    Signed-off-by: Pritam Kundu <75939390+warmachine028@users.noreply.github.com>

--- .github/take-snapshot.mjs
+++ .github/take-snapshot.mjs
@@ -2,7 +2,7 @@ import puppeteer from 'puppeteer'
 const sleep = (ms) => new Promise((res) => setTimeout(res, ms))
 
 const takeSnapShot = async () => {
-	const url = 'https://warmachine028.github.io/vite-gh' // Replace with your actual deployed URL
+	const url = 'https://warmachine028.github.io/vite-gh/' // Replace with your actual deployed URL
 	const browser = await puppeteer.launch()
 	const page = await browser.newPage()
 	await page.setViewport({ width: 1920, height: 1080 })
